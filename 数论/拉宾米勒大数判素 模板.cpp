5d963004ef9e3d4b0ee8eedf1184fa47857ee83c
WhereIsHeroFrom <menjitianya2007@163.com>
2020-12-08 18:45:11 +0800

    修改模板

--- 数论/拉宾米勒大数判素 模板.cpp
+++ 数论/拉宾米勒大数判素 模板.cpp
@@ -6,25 +6,20 @@
 #include <iostream>
 #include <cstdlib>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
-typedef __int64 Int;
-#define MAX (((Int)1)<<62)-1
-Int p[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
-Int X, Y;
-Int exp_gcd(Int a, Int b, Int &X, Int &Y)
-{
-	Int q, temp;
-	if (b == 0)
-	{
-		q = a;
-		X = 1;
-		Y = 0;
+
+typedef __int64 LL;
+LL p[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+
+LL exp_gcd(LL a, LL b, LL &X, LL &Y) {
+	LL q, temp;
+	if (!b) {
+		q = a; X = 1; Y = 0;
 		return q;
 	}
-	else
-	{
-
+	else {
 		q = exp_gcd(b, a % b, X, Y);
 		temp = X;
 		X = Y;
@@ -33,25 +28,14 @@ Int exp_gcd(Int a, Int b, Int &X, Int &Y)
 	}
 }
 
-Int Gcd(Int a, Int b)
-{
-	Int m = 1;
-	if (!b)
-		return a;
-	while (m)
-	{
-		m = a % b;
-		a = b;
-		b = m;
-	}
-	return a;
+LL Gcd(LL a, LL b) {
+	return !b ? a : Gcd(b, a%b);
 }
-//计算a*b%n
-Int Produc_Mod(Int a, Int b, Int mod)
-{
-	Int sum = 0;
-	while (b)
-	{
+
+//计算 a*b % mod
+LL Produc_Mod(LL a, LL b, LL mod) {
+	LL sum = 0;
+	while (b) {
 		if (b & 1) sum = (sum + a) % mod;
 		a = (a + a) % mod;
 		b >>= 1;
@@ -60,12 +44,10 @@ Int Produc_Mod(Int a, Int b, Int mod)
 }
 
 
-//计算a^b%n
-Int Power(Int a, Int b, Int mod)
-{
-	Int sum = 1;
-	while (b)
-	{
+//计算 a^b % mod
+LL Power(LL a, LL b, LL mod) {
+	LL sum = 1;
+	while (b) {
 		if (b & 1) sum = Produc_Mod(sum, a, mod);
 		a = Produc_Mod(a, a, mod);
 		b /= 2;
@@ -74,38 +56,40 @@ Int Power(Int a, Int b, Int mod)
 }
 
 //Rabin_Miller判素
-bool Rabin_Miller(Int n)
-{
-	Int i, j, k = 0;
-	Int u, m, buf;
-	//将n-1分解为m*2^k
+bool Rabin_Miller(LL n) {
+	LL i, j, k = 0;
+	LL u, m, buf;
+	// 1. 如果 n = 2，本身就是素数
 	if (n == 2)
 		return true;
+	// 2. n==1 或者 n是偶数，则不是素数
 	if (n < 2 || !(n & 1))
 		return false;
-	m = n - 1;
-	while (!(m & 1))
-		k++, m /= 2;
 
-	for (i = 0; i < 10; i++)
-	{
+	// 3. 将n-1分解为 m*2^k
+	// n = m*2^k + 1
+	m = n - 1;
+	while (!(m & 1)) k++, m >>= 1;
+
+	// 4. 试除10个素数
+	for (i = 0; i < 10; i++) {
+		// 4.1 n 在这些小素数当中
 		if (p[i] >= n)
 			return true;
 
+		// 4.2 满足费马小定理，则跳过检测
 		u = Power(p[i], m, n);
 		if (u == 1)
 			continue;
-		for (j = 0; j < k; j++)
-		{
-			buf = Produc_Mod(u, u, n);
-			////看是否有非平凡因子存在
 
+		// 4.3 
+		for (j = 0; j < k; j++) {
+			buf = Produc_Mod(u, u, n);
 			if (buf == 1 && u != 1 && u != n - 1)
 				return false;
 			u = buf;
 		}
-		//如果p[i]^(n-1) % n != 1 那么 n为合数
-
+		// 4.4 如果p[i]^(n-1) % n != 1 那么 n为合数
 		if (u != 1)
 			return false;
 	}
@@ -114,13 +98,12 @@ bool Rabin_Miller(Int n)
 
 //寻找n的一个因子
 
-Int Pollard_rho(Int n)
-{
-	Int i = 1;
-	Int x = rand() % (n - 1) + 1;
-	Int y = x;
-	Int k = 2;
-	Int d;
+LL Pollard_rho(LL n) {
+	LL i = 1;
+	LL x = rand() % (n - 1) + 1;
+	LL y = x;
+	LL k = 2;
+	LL d;
 	do{
 		i++;
 		d = Gcd(n + y - x, n);
@@ -129,6 +112,22 @@ Int Pollard_rho(Int n)
 		if (i == k)
 			y = x, k *= 2;
 		x = (Produc_Mod(x, x, n) + n - 23) % n;
+		if (x < 0) x += n;
 	} while (y != x);
 	return n;
+}
+
+void Factor(LL v, vector <LL>& ans) {
+	if (v == 1) {
+		return;
+	}
+
+	if (Rabin_Miller(v)) {
+		ans.push_back(v);
+		return;
+	}
+	LL f = Pollard_rho(v);
+
+	Factor(f, ans);
+	Factor(v / f, ans);
 }
