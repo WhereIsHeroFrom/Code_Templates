5dd36b87899315f35e17be14ca87b67dbfea7fee
WhereIsHeroFrom <menjitianya2007@163.com>
2021-02-18 18:17:38 +0800

    完全背包模板提交

--- 动态规划/完全背包 模板.cpp
+++ 动态规划/完全背包 模板.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 
-//************************************ 01背包 模板 ************************************
-const int MAXN = 10010;           // 物品数量 
+//************************************ 完全背包 模板 ************************************
+const int MAXN = 510;             // 物品数量 
 const int MAXC = 10010;           // 背包容量 
 typedef int ValueType;            // 背包价值的类型 
-const ValueType inf = 10000000;   // 求最大值则最小，求最小值则最大 
+const ValueType inf = 1e9;        // 求最大值则最小，求最小值则最大 
 const ValueType init = 0;         // 一般无论求最大值还是最小值，都固定为 0 
 
 ValueType dp[MAXC];
@@ -26,19 +26,19 @@ ValueType opt(ValueType x, ValueType y) {
 	return x < y ? x : y;
 }
 
-void zeroOneKnapsackInit(int maxCapacity) {
+void completeKnapsackInit(int maxCapacity) {
 	for(int i = 0; i <= maxCapacity; ++i) {
 		dp[i] = (!i) ? init : inf;
 	} 
 }
 
-void zeroOneKnapsack(int knapsackSize, Knapsack *knap, int maxCapacity) {
-	zeroOneKnapsackInit(maxCapacity);
+void completeKnapsack(int knapsackSize, Knapsack *knap, int maxCapacity) {
+	completeKnapsackInit(maxCapacity);
 	for(int i = 0; i < knapsackSize; ++i) {
-		for(int j = maxCapacity; j >= knap[i].capacity; --j) {
+		for(int j = knap[i].capacity; j <= maxCapacity; ++j) {
 			dp[j] = opt(dp[j], dp[j - knap[i].capacity] + knap[i].weight);
 		}
 	}
 }
 
-//************************************ 01背包 模板 ************************************
+//************************************ 完全背包 模板 ************************************
