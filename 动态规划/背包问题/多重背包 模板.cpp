637d36eeb6213bc059896f397dcc3e3b5feabdea
WhereIsHeroFrom <menjitianya2007@163.com>
2021-02-21 20:41:23 +0800

    模板整理一波

---
+++ 动态规划/背包问题/多重背包 模板.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+
+//************************************ 多重背包 模板 ************************************
+const int MAXN = 3000;            // 物品数量 
+const int MAXC = 110;             // 背包容量 
+typedef int ValueType;            // 背包价值的类型 
+const ValueType inf = -1e9;       // 求最大值则最小，求最小值则最大 
+const ValueType init = 0;         // 一般无论求最大值还是最小值，都固定为 0 
+
+ValueType dp[MAXC];
+
+struct Knapsack {
+	int capacity;
+	ValueType weight;
+	Knapsack(){
+	} 
+	Knapsack(int c, ValueType w) : capacity(c), weight(w) {
+	}
+}Knap[MAXN];
+
+struct MultiKnapsack : public Knapsack {
+	int count;
+	MultiKnapsack() {
+	}
+	MultiKnapsack(int cnt, int cap, ValueType w): count(cnt) {
+		capacity = cap;
+		weight = w;
+	}
+}MKnap[MAXN];
+
+ValueType opt(ValueType x, ValueType y) {
+	return x > y ? x : y;
+}
+
+void zeroOneKnapsackInit(int maxCapacity) {
+	for(int i = 0; i <= maxCapacity; ++i) {
+		dp[i] = (!i) ? init : inf;
+	} 
+}
+
+void zeroOneKnapsack(int knapsackSize, Knapsack *knap, int maxCapacity) {
+	zeroOneKnapsackInit(maxCapacity);
+	for(int i = 0; i < knapsackSize; ++i) {
+		for(int j = maxCapacity; j >= knap[i].capacity; --j) {
+			dp[j] = opt(dp[j], dp[j - knap[i].capacity] + knap[i].weight);
+		}
+	}
+}
+
+void multiKnapsack(int knapsackSize, MultiKnapsack *mknap, Knapsack *knap, int maxCapacity) {
+	int nknapSize = 0;
+	for(int i = 0; i < knapsackSize; ++i) {
+		int cnt = mknap[i].count;
+		int cap = mknap[i].capacity;
+		int val = mknap[i].weight;
+		int k = 1;
+		// 二进制拆分 
+		while( cnt ) {
+			if(k > cnt) {
+				k = cnt;
+			}
+			knap[nknapSize++] = Knapsack( cap*k, val*k );
+			cnt -= k;
+			k <<= 1;
+		}
+	}
+	zeroOneKnapsack(nknapSize, knap, maxCapacity);
+}
+
+//************************************ 多重背包 模板 ************************************
+
