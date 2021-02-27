b08caeeb8ef38ddba93a5e0c9d9a27bacbaa3c0b
WhereIsHeroFrom <menjitianya2007@163.com>
2021-02-27 09:28:46 +0800

    树上分组背包调整

--- 动态规划/背包问题/树上分组背包 模板.cpp
+++ 动态规划/背包问题/树上分组背包 模板.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 //************************************ 树上分组背包 模板 ************************************
 const int MAXN = 110;             // 物品数量 
-const int MAXC = 510;             // 背包容量 
+const int MAXC = 110;             // 背包容量 
 typedef int ValueType;            // 背包价值的类型 
 const ValueType inf = -1e9;       // 求最大值则最小，求最小值则最大 
 const ValueType init = 0;         // 一般无论求最大值还是最小值，都固定为 0 
@@ -34,33 +34,26 @@ vector <int> edges[MAXN];
 
 void treeGroupKnapsack(int father, int u, int m) {
     int i, j, k;
-    int cap[2][MAXC], pre = 0;
-
-    int maxCapacity = m - Knap[u].capacity;
-    for (i = 0; i <= maxCapacity; ++i)              // 1、分组背包容量初始化
-        cap[0][i] = 0;
-    for (i = 0; i < edges[u].size(); ++i) {         // 2、分组背包枚举组（1个子树1个组）
-        int v = edges[u][i];
-        if (v == father) continue;
-        treeGroupKnapsack(u, v, maxCapacity);       // 3、递归求解子结点所有组的状态 dp[v][0 ... capacity]
-        for (j = maxCapacity; j >= 0; --j) {        // 4、分组背包逆序枚举容量
-            cap[pre ^ 1][j] = cap[pre][j];          // 5、这个组不选物品的情况，继承自上一组
-            for (k = 0; k <= j; ++k) {               
-                                                    // 6、(容量为k, 价值为dp[v][k]) 的物品进行决策
-                                                    // 注意一定要考虑容量为 0 的情况
-                cap[pre^1][j] = opt(
-                    cap[pre^1][j],
-                    cap[pre][j - k] + dp[v][k]
-                );
+    int maxCapacity = m - Knap[u].capacity;            // 1、需要计算的最大容量
+    for (i = 0; i <= maxCapacity; ++i)                 // 2、分组背包容量初始化
+        dp[u][i] = 0;
+    for (int idx = 0; idx < edges[u].size(); ++idx) {  // 3、分组背包枚举组（1个子树1个组）
+        int v = edges[u][idx];
+        if (v == father) continue;                     // 4、无向树的处理
+        treeGroupKnapsack(u, v, maxCapacity);          // 5、递归求解子结点所有组的状态 dp[v][0 ... capacity]
+        for (i = maxCapacity; i >= 0; --i) {           // 6、分组背包逆序枚举容量
+            for (j = 0; j <= i; ++j) {                 // 7、(容量为k, 价值为dp[v][k]) 的物品进行决策
+                dp[u][i] = opt(
+                    dp[u][i],
+                    dp[u][i - j] + dp[v][j]);
             }
         }
-        pre ^= 1;
     }
-    for (i = m; i >= Knap[u].capacity; --i)         // 6、父结点必选
-        dp[u][i] = cap[pre][i - Knap[u].capacity] + Knap[u].weight;
-    for (i = 0; i < Knap[u].capacity; ++i) 
+    for (i = m; i >= Knap[u].capacity; --i)            // 8、父结点必选（不同情况不同处理）
+        dp[u][i] = dp[u][i - Knap[u].capacity] + Knap[u].weight;
+    for (i = 0; i < Knap[u].capacity; ++i)
         dp[u][i] = 0;
-    
+
     // 如果某个子结点为 0 的情况，不能走，则打开注释
     // dp[u][0] = 0;
 }
