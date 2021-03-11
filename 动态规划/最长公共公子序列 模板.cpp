f62597b95c6a10d39c416b10ead86d3387e1ef48
WhereIsHeroFrom <menjitianya2007@163.com>
2021-03-12 06:56:50 +0800

    模板

---
+++ 动态规划/最长公共公子序列 模板.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+typedef char ValueType;
+const int maxn = 2010;
+int dp[maxn][maxn];
+
+int getLCS(int hsize, ValueType *h, int vsize, ValueType *v) {
+    memset(dp, 0, sizeof(dp));
+    dp[0][0] = 0;
+    for (int i = 1; i <= vsize; ++i) {
+        for (int j = 1; j <= hsize; ++j) {
+            if (v[i] == h[j]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[vsize][hsize];
+}
+
+char s[2][maxn];
+
+int main() {
+    int len[2];
+    while (scanf("%s %s", &s[0][1], &s[1][1]) != EOF) {
+        len[0] = strlen(&s[0][1]);
+        len[1] = strlen(&s[1][1]);
+        printf("%d\n", getLCS(len[0], s[0], len[1], s[1]));
+
+    }
+    return 0;
+}
