cc40bc16bbcaa2c205c34f8ce3744aee9d1856f4
WhereIsHeroFrom <menjitianya2007@163.com>
2021-05-11 05:00:01 +0800

    二分快速幂 模板修改

--- 数论/二分快速幂.cpp
+++ 数论/二分快速幂.cpp
@@ -1,14 +1,15 @@
-#define LL long long
+#define ll long long
 
-LL Exp(LL a, LL n, LL Mod){
-	LL ans = 1;
+ll Exp(ll a, ll n, ll Mod){
+	ll ans = 1;
 	if (a == 0 || a == 1) {
-		return a;
+		return a % Mod;
 	}
 	while (n){
-		if (n & 1) ans = ans*a%Mod;
-		a = a*a%Mod;
+		if (n & 1) ans = ans * a % Mod;
+		a = a * a % Mod;
 		n >>= 1;
 	}
 	return ans;
 }
+
