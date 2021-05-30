0033656be13932cff835ec90a73f9381c7a1fbd4
WhereIsHeroFrom <menjitianya2007@163.com>
2021-05-31 07:28:59 +0800

    扩展欧拉定理 模板

--- 数论/扩展欧拉定理 模板.cpp
+++ 数论/扩展欧拉定理 模板.cpp
@@ -1,25 +1,28 @@
 /*
-欧拉函数：
-n分解素因子表示为n = p1^e1 * p2^e2 * ... * pk^ek
-则n的欧拉函数为 f(n) = (p1-1)p1^(e1-1) * (p2-1)p2^(e2-1) * ... * (pk-1)pk^(ek-1)
-
-扩展欧拉定理：
-a^b % c
-当 b >= phi(c), a^b % c = a^(b % phi(c) + phi(c)) % c
-
-Author: WhereIsHeroFrom
-Update Time: 2020-10-29
-Algorithm Complexity: O(sqrt(n))
-
+	扩展欧拉定理前置三要素
+		1、素数筛选
+		2、欧拉函数求解
+		3、二分快速幂
+	当 b >= phi(c) 时
+		a^b mod c = a^(b mod phi(c) + phi(c)) mod c
 */
 
 #include <iostream>
+#include <queue>
 #include <cstring>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
 #include <vector>
+#include <memory.h>
+#include <cmath>
 using namespace std;
 
+#pragma warning(disable : 4996)
+
+#define ll long long
 #define MAXP 65540
-#define LL __int64
 
 int primes[MAXP];
 bool notprime[MAXP];
@@ -32,8 +35,8 @@ void Eratosthenes() {
 	for (int i = 2; i < MAXP; i++) {
 		if (!notprime[i]) {
 			primes[++primes[0]] = i;
-			//需要注意i*i超出整型后变成负数的问题，所以转化成 __int64 
-			for (LL j = (LL)i*i; j < MAXP; j += i) {
+			//需要注意i*i超出整型后变成负数的问题，所以转化成 long long
+			for (ll j = (ll)i * i; j < MAXP; j += i) {
 				notprime[j] = true;
 			}
 		}
@@ -44,48 +47,32 @@ void Eratosthenes() {
 // 举例：
 // Phi(10) = 4
 // 即 1、3、7、9  总共4个 
-LL Phi(LL n) {
-	if (n == 1) {
+int Phi(int n) {
+	if (n == 1)
 		return 1;
-	}
-	LL ans = 1;
-	// 素数试除 
+	int ans = 1;
 	for (int i = 1; i <= primes[0]; i++) {
-		LL p = primes[i];
-		if (n % p == 0) {
+		int p = primes[i];
+		if (0 == n % p) {
 			n /= p;
 			ans *= (p - 1);
-			while (!(n % p)) {
+			while (0 == n % p) {
 				n /= p;
 				ans *= p;
 			}
 		}
-		if (n == 1) {
+		if (n == 1)
 			return ans;
-		}
 	}
 	return ans * (n - 1);
 }
 
-LL Exp(LL a, LL b, LL c) {
-	if (b == 0) {
-		return 1 % c;
+ll Exp(ll a, ll n, ll Mod) {
+	ll ans = 1;
+	while (n) {
+		if (n & 1) ans = ans * a % Mod;
+		a = a * a % Mod;
+		n >>= 1;
 	}
-	LL tmp = Exp(a*a%c, (b >> 1), c);
-	if (b & 1) {
-		tmp = tmp * a % c;
-	}
-	return tmp;
-}
-
-// 扩展欧拉函数计算 a^b MOD c
-LL ExtendPhi(LL a, LL b, LL c) {
-	LL ph = Phi(c);
-	if (b <= ph) {
-		return Exp(a, b, c);
-	}
-
-	// 实际用到欧拉函数计算的时候
-	// b 往往是一个很大的数，且 b 是个表达式，是矩阵或者递推式
-	return Exp(a, b % ph + ph, c);
+	return ans;
 }
