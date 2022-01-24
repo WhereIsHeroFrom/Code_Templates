0451b8025c5b2bfda8b58a80fe5ca5f8c176ca03
WhereIsHeroFrom <34783332+WhereIsHeroFrom@users.noreply.github.com>
2022-01-25 07:27:59 +0800

    增加朴素回文串判定接口

---
+++ 字符串/朴素回文串.c
@@ -0,0 +1,33 @@
+#define maxn 1010
+bool isPal[1010][1010];
+
+void preCalc(char *s) {
+    int i, j;
+    int len = strlen(s);
+    for(i = 0; i < len; ++i) {
+        // 奇数
+        for(j = 0; j < len; ++j) {
+            l = i-j;
+            r = i+j;
+            if(l < 0 || r >= len) {
+                break;
+            }
+            if(l == r) {
+                isPal[l][r] = true;
+            }else
+                isPal[l][r] = ( (s[l] == s[r]) && isPal[l+1][r-1] );
+        }
+        // 偶数
+        for(j = 0; j < len; ++j) {
+            l = i-j;
+            r = i+1+j;
+            if(l < 0 || r >= len) {
+                break;
+            }
+            if(l + 1 == r) {
+                isPal[l][r] = (s[l] == s[r]);
+            }else
+                isPal[l][r] = (s[l] == s[r]) && isPal[l+1][r-1];
+        }
+    }
+}
